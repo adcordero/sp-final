b3cdffea2dcf4d0341bbf3f161a7ef1554e37236
Danielle Cordero <84511937+adcordero@users.noreply.github.com>
2024-05-20 20:51:08 +0800

    feat: signup backend done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 
 import React, { useState } from 'react'
+import { FaEye } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import supabase from '../configs/supabaseClient'
 // import { Dropdown } from 'react-bootstrap'
 
 const SignUp = () => {
@@ -13,24 +15,100 @@ const SignUp = () => {
     email_add: '',
     password: '',
     birthday: '',
-    contact_num: ''
+    contact_num: '',
   })
 
   const [confirmPass, setConfirmPass] = useState('')
+  const [invalidPass, setInvalidPass] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [showPass, setShowPass] = useState(false)
+  const [showRetyped, setShowRetyped] = useState(false)
 
-  // function 
+  function handleShowPass() {
+    setShowPass(!showPass)
+  }
 
-  console.log(formData)
+  function handleShowRetyped() {
+    setShowRetyped(!showRetyped)
+  }
+
+  async function handleSubmit(event) {
+
+    event.preventDefault()
+
+    if (formData.password === confirmPass) {
+      try {
+        const { data, error } = await supabase.auth.signUp(
+          {
+            email: formData.email_add,
+            password: formData.password,
+            options: {
+              data: {
+                first_name: formData.first_name,
+                middle_name: formData.middle_name,
+                last_name: formData.last_name,
+                sex: formData.sex,
+                birthday: formData.birthday,
+                contact_num: formData.contact_num,
+              }
+            }
+          }
+        )
+        alert('Check email address for confirmation.')
+      } catch (error) {
+        alert(error)
+      }
+    } else {
+      alert('Passwords do not match.')
+    }
+  }
+
+  // console.log(formData)
 
   function handleChange(event) {
+
     setFormData((prevFormData)=> {
       return{
         ...prevFormData,
         [event.target.name]: event.target.value
+        
       }
     })
   }
 
+  function checkPasswordValidity(event) {
+    const passRegEx = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+-])(?!.* ).{8,16}$/
+    
+
+    if (event.target.value == '') {
+      setInvalidPass('')
+    } else if (!passRegEx.test(event.target.value)) {
+      setInvalidPass('Password invalid.')
+    } else {
+      setInvalidPass('')
+    }
+
+    setFormData((prevFormData)=> {
+      return{
+        ...prevFormData,
+        [event.target.name]: event.target.value
+        
+      }
+    })
+  }
+
+  function confirmRetypedPass(event) {
+
+    setConfirmPass(event.target.value)
+
+    if (event.target.value == '') {
+      setPasswordConfirm('')
+    } else if (event.target.value != formData.password) {
+      setPasswordConfirm('Does not match.')
+    } else {
+      setPasswordConfirm('')
+    }
+  }
 
   return (
     <main className={`h-screen w-full flex items-center justify-center`} >
@@ -44,12 +122,12 @@ const SignUp = () => {
           
         </div>
 
-        <form className={`flex flex-col items-center gap-5`}>
+        <form className={`flex flex-col items-center gap-5`} onSubmit={handleSubmit}>
 
           <div className={`w-full flex gap-2`}>
             <div className={`flex flex-col gap-2 w-2/4`}>
               <label className={`text-sm font-inter font-bold pl-1`}>First Name</label>
-              <input placeholder='First Name' name='first_name' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} />
+              <input placeholder='First Name' name='first_name' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required />
             </div>
 
             <div className={`flex flex-col gap-2 w-1/4`}>
@@ -59,7 +137,7 @@ const SignUp = () => {
 
             <div className={`flex flex-col gap-2 w-1/4`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Last Name</label>
-              <input placeholder='Last Name' name='last_name' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} />
+              <input placeholder='Last Name' name='last_name' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required />
             </div>
             
           </div>
@@ -67,7 +145,8 @@ const SignUp = () => {
           <div className={`w-full flex gap-2`}>
             <div className={`flex flex-col gap-2 w-1/3`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Sex</label>
-              <select name='sex' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange}>
+              <select name='sex' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required>
+                <option value=''>Select option</option>
                 <option value='male'>Male</option>
                 <option value='female'>Female</option>
               </select>
@@ -75,29 +154,32 @@ const SignUp = () => {
 
             <div className={`flex flex-col gap-2 w-1/3`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Contact Number</label>
-              <input type="tel" id="phone" pattern="[0-9]{4}-[0-9]{3}-[0-9]{4}" placeholder='09*********' name='contact_num' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} />
+              <input type="tel" id="phone" pattern="[0-9]{4}-[0-9]{3}-[0-9]{4}" placeholder='09**-***-****' name='contact_num' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required />
             </div>
 
             <div className={`flex flex-col gap-2 w-1/3`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Birthday</label>
-              <input type='date' placeholder='MM/DD/YYYY' name='birthday' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} />
+              <input type='date' placeholder='MM/DD/YYYY' name='birthday' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required />
             </div>
           </div>
           
           <div className={`w-full flex gap-2`}>
             <div className={`flex flex-col gap-2 w-2/4`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Email Address</label>
-              <input type='email' placeholder='Email Address' name='email_add' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} />
+              <input type='email' placeholder='Email Address' name='email_add' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} required />
             </div>
 
             <div className={`flex flex-col gap-2 w-1/4`}>
               <label className={`text-sm font-inter font-bold pl-1`}>Password</label>
-              <input type='password' placeholder='Password' name='password' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={handleChange} minLength='8' maxLength='16' pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$" />
+              <input type='password' placeholder='Password' name='password' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={checkPasswordValidity} minLength='8' maxLength='16' pattern='/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+-])(?!.* ).{8,16}$/' required  />
+              {/* <FaEye /> */}
+              <p className={`text-sm font-inter font-bold pl-1`}>{invalidPass}</p>
             </div>
 
             <div className={`flex flex-col gap-2 w-1/4`}>
-              <label className={`text-sm font-inter font-bold pl-1`}>Confirm Password</label>
-              <input type='password' placeholder='Password' name='confirmPass' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={(event) => setConfirmPass(event.target.value)} minLength='8' maxLength='16' pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$" />
+              <label className={`text-sm font-inter font-bold pl-1`}>Confirm</label>
+              <input type='password' placeholder='Password' name='confirmPass' className={`focus:outline-none rounded-sm border-black border-2 p-2 text-sm`} onChange={confirmRetypedPass} minLength='8' maxLength='16' required />
+              <p className={`text-sm font-inter font-bold pl-1`}>{passwordConfirm}</p>
             </div>
           </div>
 
